19320333efe9892f40ff9cfabe91ee1191870665
reverieriha <marihamahmood2003@gmail.com>
2025-04-02 22:33:48 -0400

    Add files via upload

---
+++ final project/Restart.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Restart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        }
+    }
+}
